c825a66090d8cea17c65b4f4933c32f333587223
Sai srikar <saisrikar1220@gmail.com>
2026-09-17 06:27:49 +0530

    Time: 423 ms (82.03%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,4 @@
-SELECT DISTINCT author_id AS id FROM Views
-WHERE author_id = viewer_id ORDER BY id ASC;
+SELECT DISTINCT author_id AS id 
+FROM Views 
+WHERE author_id = viewer_id 
+ORDER BY author_id asc;
